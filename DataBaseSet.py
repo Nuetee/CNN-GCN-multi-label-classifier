f1173f3d6be49f09c0e461db6d8676bc05838b74
Nuetee <sungjoon729@gmail.com>
2022-10-19 20:20:22 +0900

    style, genre, media를 조합하여 파일 이름 생성
    Former-commit-id: 218359fe0f24c51a7adef090e6cbaefd7cc8b7f0

--- DataBaseSet.py
+++ DataBaseSet.py
@@ -9,7 +9,7 @@ with open('./wikiart.json', 'r', encoding='UTF-8') as f:
     json_data = json.load(f)
 
 data_list = json_data[2]["data"]
-data_list = data_list[15642:]
+# data_list = data_list[159691:]
 
 # image_name = './database/NASA3 .jpg'
 # f = open(image_name, 'wb')
@@ -41,32 +41,32 @@ for data in data_list:
     jsonObject = json.loads(core_data)
     
     image_id = data["image_id"]
-    title = ''
-    style = None
+    genre = ''
+    media = ''
+    style = ''
 
     imageURL = jsonObject["image"] if 'image' in jsonObject else None
     if imageURL == None or imageURL == 'error':
         no_download_count += 1
         continue
         
-    title = jsonObject["title"] if 'title' in jsonObject else ''
-    style = jsonObject["style"] if 'style' in jsonObject else None
-    if style == None:
-        no_download_count += 1
-        continue
+    genre = jsonObject["genre"] if 'genre' in jsonObject else ''
+    style = jsonObject["style"] if 'style' in jsonObject else ''
+    media = jsonObject["media"] if 'media' in jsonObject else ''
     
 
     for character in replace_character:
-        title = title.replace(character, '')
+        genre = genre.replace(character, '')
         style = style.replace(character, '')
+        media = media.replace(character, '')
     # if image_id == '11928':
     #     title = 'Map of the Northern Sky with representations of the constellations'
-    image_name = image_id + '+' + title + '+' + style + '.jpg'
-    if len(image_name) > 198:
-        over = len(image_name) - 198
-        title = title[0:(len(title) - over)]
-        image_name = image_id + '+' + title + '+' + style + '.jpg'
-    file_path = './database_style/' + image_name
+    image_name = image_id + '+' + genre + '+' + style + '+'  + media + '.jpg'
+    
+    if len(image_name) > 200:
+        continue
+
+    file_path = './database_all/' + image_name
     # print(image_name)
     # print(imageURL)
     response = requests.get(imageURL)
